5aec3b45e6759b9112803ec97043a9011be10d03
Deathruin <91310998+Deathruin@users.noreply.github.com>
2021-11-26 16:13:54 +0800

    Five1

--- src/java9/Five9_11.java
+++ src/java9/Five9_11.java
@@ -1,5 +1,20 @@
 package java9;
+import java.util.Scanner;
 
 public class Five9_11 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a,b,c,d,e,f:");
+		double a =input.nextInt(), b =input.nextInt(), c =input.nextInt();
+		double d =input.nextInt(), e =input.nextInt(), f =input.nextInt();
+		
+		Five9_11_linearEquation le = new Five9_11_linearEquation(a,b,c,d,e,f);
+		boolean bool = le.isSolvable();
+		
+		if(bool) {
+			System.out.println("x is " + le.getX() + " and y is " + le.getY());
+		}else
+			System.out.println("The equation has no solution");
+	}
 
 }
